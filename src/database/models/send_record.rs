23e39ace4d51dc4aaa6a04a33c38d70ca6c0a778
Anna <git@anna.lgbt>
2017-10-15 13:37:29 -0400

    refactor: remove id from send records

--- src/database/models/send_record.rs
+++ src/database/models/send_record.rs
@@ -1,15 +1,18 @@
 use database::schema::*;
 use database::models::news_item::NewsItem;
+use database::models::server::Server;
 
-insertable! {
-  #[derive(Debug, Queryable, Identifiable, Associations)]
-  // #[belongs_to(Server)]
-  #[belongs_to(NewsItem, foreign_key = "news_id")]
-  pub struct SendRecord,
-  #[derive(Debug, Insertable)]
-  #[table_name = "send_records"]
-  pub struct NewSendRecord {
-    pub server_id: i32,
-    pub news_id: i32
-  }
+#[derive(Debug, Queryable, Associations)]
+#[belongs_to(NewsItem, foreign_key = "news_id")]
+#[belongs_to(Server, foreign_key = "server_id")]
+pub struct SendRecord {
+  pub server_id: i32,
+  pub news_id: i32
+}
+
+#[derive(Debug, Insertable)]
+#[table_name = "send_records"]
+pub struct NewSendRecord {
+  pub server_id: i32,
+  pub news_id: i32
 }
