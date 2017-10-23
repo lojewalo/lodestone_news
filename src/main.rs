d6b1cf4cd4a5fce06ddf3fc4ffa9357f93ee6d40
Kyle Clemens <jkc.clemens@gmail.com>
2017-10-23 14:13:12 -0400

    refactor: use chan-signal

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,10 @@ extern crate diesel_codegen;
 extern crate error_chain;
 #[macro_use]
 extern crate log;
-extern crate ctrlc;
 #[macro_use]
 extern crate chan;
+extern crate chan_signal;
+extern crate bus;
 extern crate fern;
 extern crate ansi_term;
 
@@ -36,6 +37,7 @@ use diesel::connection::SimpleConnection;
 use diesel::sqlite::SqliteConnection;
 
 use chan::{async, tick};
+use chan_signal::Signal;
 
 use chrono::Duration;
 
@@ -58,23 +60,32 @@ fn main() {
 
   dotenv::dotenv().ok();
 
+  info!("Setting Ctrl-C channel");
+
+  let (exit_tx, exit_rx) = async();
+  chan_signal::notify_on(&exit_tx, Signal::INT);
+
   info!("Creating channels and tickers");
 
   let (ts_tx, ts_rx) = async();
   let ns_tick = tick(Duration::seconds(150).to_std().unwrap());
   let ds_tick = tick(Duration::minutes(5).to_std().unwrap());
 
-  info!("Setting Ctrl-C handler");
+  let mut thread_handles = Vec::new();
 
-  ctrlc::set_handler(move || {
-    ts_tx.send(());
-    ts_tx.send(());
-  }).unwrap();
+  info!("Starting exit thread");
+
+  thread_handles.push(std::thread::spawn(move || {
+    exit_rx.recv().unwrap();
+    for _ in 0..2 {
+      ts_tx.send(());
+    }
+  }));
 
   info!("Starting scraper thread");
 
   let ns_ts_rx = ts_rx.clone();
-  let news_scraper_handle = ::std::thread::spawn(move || {
+  thread_handles.push(std::thread::spawn(move || {
     let scraper = lodestone::NewsScraper::new();
     loop {
       chan_select! {
@@ -85,11 +96,11 @@ fn main() {
         warn!("Could not update Lodestone news: {}", e);
       }
     }
-  });
+  }));
 
   info!("Starting Discord thread");
 
-  let discord_sender_handle = ::std::thread::spawn(move || {
+  thread_handles.push(std::thread::spawn(move || {
     let ds = discord::DiscordSender::new();
     loop {
       chan_select! {
@@ -100,12 +111,13 @@ fn main() {
         warn!("Could not send Discord news: {}", e);
       }
     }
-  });
+  }));
 
   info!("Waiting on joins");
 
-  news_scraper_handle.join().unwrap();
-  discord_sender_handle.join().unwrap();
+  for handle in thread_handles {
+    handle.join().unwrap();
+  }
 
   info!("Done");
 }
